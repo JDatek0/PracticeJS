ab9f9c2d8f12877f81f72547fc515183847ce2a4
JDatek0 <jorge.dtk0@gmail.com>
2022-10-24 18:02:25 -0500

    Esta es la primera parte de la prueba hasta las funciones.

---
+++ pruebajs.js
@@ -0,0 +1,35 @@
+var nombre = "Jorge";
+var apellido = "Tamayo";
+var nombreUsuarioPlatzi = "Jorge tamayo";
+var edad = "24"
+var email = "jorge.dtk0@gmail.com"
+var mayorEdad = true;
+var dineroAhorrado = 6000;
+var deudas = 563;
+
+var nombreCompleto = nombre + apellido;
+var dineroReal =  dineroAhorrado - deudas;
+
+console.log("Tu nombre completo es: " + nombreCompleto+ " Y tu dinero real en el banco es: " + dineroReal+" Pesos Mexicanos");
+
+//Convierte el siguiente código en una función, pero, cambiando cuando sea necesario las variables constantes por parámetros y argumentos en una función:
+
+//const name = "Juan David";
+//const lastname = "Castro Gallego";
+//const completeName = name + lastname;
+//const nickname = "juandc";
+
+//console.log("Mi nombre es " + completeName + ", pero prefiero que me digas " + nickname + ".");
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var namee = "jorge carlos";
+var lastName="Tamayo"
+var nickname ="Datek0"
+
+function llamarPersona(namee,lastName,nickname){
+    completeName = namee +" "+ lastName 
+    
+    console.log("Me llamo : " + completeName + ", pero prefiero que me digas "+ nickname + ".")
+};
+
