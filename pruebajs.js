02e1a36c1696b34a2c8766d8f0af1de1cd32c740
JDatek0 <jorge.dtk0@gmail.com>
2022-10-24 19:24:28 -0500

    Casi termino condicionales, me falto el ultimo ejercicio

--- pruebajs.js
+++ pruebajs.js
@@ -11,6 +11,7 @@ var nombreCompleto = nombre + apellido;
 var dineroReal =  dineroAhorrado - deudas;
 
 console.log("Tu nombre completo es: " + nombreCompleto+ " Y tu dinero real en el banco es: " + dineroReal+" Pesos Mexicanos");
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //Convierte el siguiente código en una función, pero, cambiando cuando sea necesario las variables constantes por parámetros y argumentos en una función:
 
@@ -32,4 +33,37 @@ function llamarPersona(namee,lastName,nickname){
     
     console.log("Me llamo : " + completeName + ", pero prefiero que me digas "+ nickname + ".")
 };
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//const tipoDeSuscripcion = "Basic";
+
+//switch (tipoDeSuscripcion) {
+  // case "Free":
+    //   console.log("Solo puedes tomar los cursos gratis");
+      // break;
+   //case "Basic":
+       //console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+  //     break;
+   //case "Expert":
+     //  console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+//       break;
+   //case "ExpertPlus":
+     //  console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+      // break;
+//}
+const tipoDeSuscripcion = "Expert";
+if(tipoDeSuscripcion === "Free"){
+    console.log("solo puedes tomar los cursos que son gratis")
+} else if(tipoDeSuscripcion === "Basic"){
+    console.log("solo puedes tomar los cursos que son grais")
+}else if(tipoDeSuscripcion === "Expert"){
+    console.log("Puedes tomar casi todos los cursos de Platzi durante un año")
+}else{console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año")}
+
+
+
+
+
+
 
