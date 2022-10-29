f077a7fcbfdc01b9f243dccea4b62a7bf3fba67c
JDatek0 <jorge.dtk0@gmail.com>
2022-10-28 19:46:13 -0500

    Esta es la primera implementacion de manipulacion del dom

--- jsMain.js
+++ jsMain.js
@@ -1,32 +1,11 @@
-const h1 = document.querySelector('h1');
-const p = document.querySelectorAll('p');
-const parrafo2 = document.querySelector('.parrafo2');
-const messi = document.querySelector('#messi');
-const input = document.querySelector('input');
+const h1 = document.querySelector('h1')
+const inp1 = document.querySelector('#inpNumero1');
+const inp2 = document.querySelector('#inpNumero2');
+const btnCalcular = document.querySelector('#btnCalcular'); 
+const h3Resultl = document.querySelector('#result');
 
-console.log(input.value)
-console.log({
-
-    h1,
-    p,
-    parrafo2,
-    messi,
-    input
-})
-
-h1.innerText = 'mesiculeiro <p>Joselito Pies de plata</p>';
-
-// console.log(h1.getAttribute('equipo'))
-// h1.setAttribute('equipo','RM')
-
-h1.classList.add('RM')
-h1.classList.remove('PSG')
-input.placeholder = "venedicto"
-
-const imagenarch = document.createElement('img');
-imagenarch.setAttribute('src','./arc.png')
-
-imagenarch.setAttribute('width','200px',);
-imagenarch.setAttribute('height','100px');
-parrafo2.replaceWith(imagenarch)
-
+function btnOnClick(){
+    let resultado;
+    resultado = parseInt(inp1.value ) + parseInt( inp2.value);
+    h3Resultl.textContent = resultado;
+}
